8f52a9ef173701cb85f88ff0af8bc115e096071f
amran-muhammad <amran8258@gmail.com>
2024-10-14 23:37:05 +0600

    api changed

--- api/chat.js
+++ api/chat.js
@@ -1,28 +1,29 @@
 import { Server } from "socket.io";
 
-export default function (req, res) {
-  const io = new Server(res.socket.server);
-  if (res.socket.server.io) {
-    console.log("Socket is already running");
-    return res.socket.server.io;
+let io;
+
+export default function handler(req, res) {
+  if (!io) {
+    const server = res.socket.server;
+    io = new Server(server);
+
+    io.on("connection", (socket) => {
+      console.log("A user connected");
+
+      socket.on("register", (username) => {
+        socket.username = username;
+      });
+
+      socket.on("chat message", (msg) => {
+        io.emit("chat message", { user: socket.username, message: msg });
+      });
+
+      socket.on("disconnect", () => {
+        console.log("A user disconnected");
+      });
+    });
   }
 
-  io.on("connection", (socket) => {
-    console.log("A user connected");
-
-    socket.on("register", (username) => {
-      socket.username = username;
-    });
-
-    socket.on("chat message", (msg) => {
-      io.emit("chat message", { user: socket.username, message: msg });
-    });
-
-    socket.on("disconnect", () => {
-      console.log("A user disconnected");
-    });
-  });
-
   res.socket.server.io = io;
-  return res.end();
+  res.end();
 }
